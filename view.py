a746f9ab969ba8bee7f6b6e8efdd3a82d5e4529d
vassi <asrtjlbv@ffgh.com>
2023-04-10 18:01:14 +0300

    m,m..mm

--- view.py
+++ view.py
@@ -1,16 +1,28 @@
 import pygame
-screen = pygame.display.set_mode([200, 600])
-import random,model
+
+screen = pygame.display.set_mode([200,300])
+import random, model
 
 a = pygame.image.load('images/water_drop.png')
 f = pygame.image.load('images/flower1.png')
-g= pygame.image.load('images/flower2.png')
+g = pygame.image.load('images/flower2.png')
+
+x_fold = f.get_height() / f.get_width()
+
 def draw():
     global screen
-    if model.magloire.bottom>screen.get_height():
-        pygame.display.set_mode([screen.get_width()+1,screen.get_height()])
+    if model.magloire.bottom > screen.get_height():
+        pygame.display.set_mode([screen.get_width() + 1, screen.get_height()])
+    z_fold = screen.get_width() / 2
+    if x_fold * z_fold<=screen.get_height():
+        c = pygame.transform.scale(f, [z_fold, x_fold * z_fold])
+    else:
+        c=pygame.transform.scale(f, [z_fold, screen.get_height()])
+    b = pygame.transform.scale(a, [model.magloire.width, model.magloire.height])
+
     screen.fill([0, 0, 0])
-    b=pygame.transform.scale(a,[model.magloire.width,model.magloire.height])
-    screen.blit(b , [model.magloire.left,model.magloire.top])
+
+    screen.blit(c, [0, screen.get_height() - c.get_height()])
+    screen.blit(b, [model.magloire.left, model.magloire.top])
 
     pygame.display.flip()
